f4e6ae074cbcbd9007a01f95dd20a4727abfaaa8
SevenRc <frank.restrepo@uao.edu.co>
2023-10-12 12:38:10 -0500

    finish web page
    Finish web page

--- Kick/app.js
+++ Kick/app.js
@@ -1,67 +1,32 @@
-let horas = 7;
-let minutos = 59;
-let segundos = 0;
-CargarSegundos();
+// Obtenemos los elementos del DOM que vamos a actualizar
+const diasElement = document.getElementById('dias');
+const horasElement = document.getElementById('horas');
+const minutosElement = document.getElementById('minutos');
+const segundosElement = document.getElementById('segundos');
 
-function CargarSegundos(){
-    let txtSegundos;
-    if(segundos < 0){
-        segundos = 59
-    }
+// Definimos la fecha límite para el contador
+const fechaLimite = new Date('October 14, 2023 00:00:00');
+console.log(fechaLimite);
 
-    if(segundos < 10){
-        txtSegundos = `0${segundos}`
-    }else{
-        txtSegundos = segundos;
-    }
-    document.getElementById('segundos').innerHTML = txtSegundos;
-    segundos --;
+function actualizarContador() {
+  // Obtenemos la fecha y hora actual
+  const fechaActual = new Date();
 
-    CargarMinutos(segundos);
+  // Calculamos la diferencia entre la fecha límite y la fecha actual
+  const diferencia = fechaLimite - fechaActual;
+
+  // Calculamos los días, horas, minutos y segundos restantes
+  const dias = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+  const horas = Math.floor((diferencia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutos = Math.floor((diferencia % (1000 * 60 * 60)) / (1000 * 60));
+  const segundos = Math.floor((diferencia % (1000 * 60)) / 1000);
+
+  // Actualizamos los elementos del DOM con los valores calculados
+  diasElement.textContent = dias < 10 ? `0${dias}` : dias;
+  horasElement.textContent = horas < 10 ? `0${horas}` : horas;
+  minutosElement.textContent = minutos < 10 ? `0${minutos}` : minutos;
+  segundosElement.textContent = segundos < 10 ? `0${segundos}` : segundos;
 }
 
-function CargarMinutos(segundos){
-    let txtMinutos;
-
-    if(segundos == -1 && minutos !==0){
-        setTimeout(()=>{
-            minutos--;
-        }, 500)
-    }else if(segundos == -1 && minutos !==0){
-        setTimeout(()=>{
-            minutos= 59;
-        },500)
-    }
-
-    if(minutos < 10){
-        txtMinutos = `0${minutos}`;
-    }else{
-        txtMinutos = minutos;
-    }
-    document.getElementById('minutos').innerHTML = txtMinutos;
-    CargarHoras(segundos, minutos);
-}
-
-function CargarHoras(segundos, minutos){
-    let txtHoras;
-
-    if(segundos == -1 && minutos == 0 && horas !== 0){
-        setTimeout(()=>{
-            horas--;
-        },500)
-    }else if(segundos == -1 && minutos == 0 && horas == 0){
-        setTimeout(()=>{
-            horas = 2;
-        },500)
-    }
-
-    if(horas < 10){
-        txtHoras = `0${horas}`;
-    }else{
-        txtHoras - horas;
-    }
-
-    document.getElementById('horas').innerHTML = txtHoras;
-}
-
-setInterval(CargarSegundos, 1000);
+// Actualizamos el contador cada segundo
+setInterval(actualizarContador, 1000);
